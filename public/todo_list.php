7ff70151b57d268a59bb1bb94a49a02982365354
Jamie Haskell <jmehaskell@gmail.com>
2015-02-09 15:32:13 -0600

    periodic commit

--- public/todo_list.php
+++ public/todo_list.php
@@ -5,9 +5,11 @@
 <html>
 	<head>
 		<title>TO DO List</title>
+		<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/css/site.css">
 	</head>
 	<body>
-		<h1>TO DO List</h1>
+		<h1 class="header">TO DO List</h1>
 			<ul>
 				<li>Sweep tile</li>
 				<li>Wash dishes</li>
@@ -17,12 +19,12 @@
 			<br>
 			<br>
 			<br>
-		<h2>Add an Item to the List</h2>
-			<form method="POST" action="/todo_list.php">
+		<h2 class="header">Add an Item to the List</h2>
+			<form method="POST" action="/todo_list.php" class="header">
 				<label for="todo_list">Enter an item to add:
 					<input id="todo_list" name="added item:" type="text">
 				</label>
-				<input type="submit" value="Add">
+				<input id="addButton" type="submit" value="Add">
 			</form>
 	</body>
 </html>
